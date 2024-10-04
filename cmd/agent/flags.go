7d8c6d0fb6d13f6a6fbe86331986dae690c81e19
Mikhail Solovev <mixailo@mixailo.org>
2024-10-04 17:55:31 +0300

    iteration 4: add command line params

---
+++ cmd/agent/flags.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type endpoint struct {
+	host string
+	port int
+}
+
+func (e *endpoint) String() string {
+	return fmt.Sprintf("%s:%d", e.host, e.port)
+}
+
+func (e *endpoint) Set(value string) error {
+	items := strings.Split(value, ":")
+	if len(items) != 2 {
+		return errors.New("Invalid format")
+	}
+
+	e.host = items[0]
+	e.port, _ = strconv.Atoi(items[1])
+
+	return nil
+}
+
+var pollInterval int64
+var reportInterval int64
+var serverEndpoint = endpoint{host: "127.0.0.1", port: 8080}
+
+func ParseFlags() {
+	flag.Var(&serverEndpoint, "a", "server endpoint")
+	flag.Int64Var(&pollInterval, "p", 2, "poll interval")
+	flag.Int64Var(&reportInterval, "r", 10, "report interval")
+
+	flag.Parse()
+}
