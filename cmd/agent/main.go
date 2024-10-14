256ed225ee24be76312577cf4696aa65435c2318
Mikhail Solovev <mixailo@mixailo.org>
2024-10-14 20:59:09 +0300

    fix: change agent main loop

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/mixailo/go-training-metrics/internal/service/logger"
 	"go.uber.org/zap"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/mixailo/go-training-metrics/internal/service/metrics"
@@ -14,34 +16,53 @@ import (
 var totalPolls int64
 var report metrics.Report
 
+func gracefulShutdown(done chan bool) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		done <- true
+	}()
+}
+
 func main() {
 	agentConf := initConfig()
 
 	logger.Initialize("info")
-
 	logger.Log.Info("Starting agent")
-	logger.Log.Info(fmt.Sprint(agentConf))
-	lastPoll := time.Now()
-	lastReport := time.Now()
 
 	reportEndpoint := sender.NewServerEndpoint("http", agentConf.endpoint.Host, agentConf.endpoint.Port)
-	time.Sleep(2 * time.Second)
+
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+	done := make(chan bool)
+
+	go gracefulShutdown(done)
+
 	for {
 		currentTime := time.Now()
-		if time.Since(lastPoll) >= time.Duration(agentConf.pollInterval)*time.Second {
-			report = poller.PollMetrics()
-			totalPolls += 1
-			lastPoll = currentTime
-		}
-		if time.Since(lastReport) >= time.Duration(agentConf.reportInterval)*time.Second {
-			report.Add(metrics.Metrics{ID: "PollCount", MType: metrics.TypeCounter.String(), Delta: &totalPolls})
-			err := sender.SendReport(report, reportEndpoint)
-			if err != nil {
-				logger.Log.Info("error", zap.Error(err))
+		lastPoll := currentTime
+		lastReport := currentTime
+
+		select {
+		case <-done:
+			fmt.Println("Done, shutting down gracefully!")
+			return
+		case _ = <-ticker.C:
+			if time.Since(lastPoll) >= time.Duration(agentConf.pollInterval)*time.Second {
+				report = poller.PollMetrics()
+				totalPolls += 1
+				lastPoll = currentTime
 			}
-			lastReport = currentTime
-			totalPolls = 0
+			if time.Since(lastReport) >= time.Duration(agentConf.reportInterval)*time.Second {
+				report.Add(metrics.Metrics{ID: "PollCount", MType: metrics.TypeCounter.String(), Delta: &totalPolls})
+				err := sender.SendReport(report, reportEndpoint)
+				if err != nil {
+					logger.Log.Info("error", zap.Error(err))
+				}
+				lastReport = currentTime
+				totalPolls = 0
+			}
 		}
-		time.Sleep(100 * time.Millisecond)
 	}
 }
